0f8d416020d406b5050397dda50793ac25b2aa05
cj <cj@mosynx.com>
2022-09-22 22:58:29 +0800

    优化和完善

--- app/common/spider.py
+++ app/common/spider.py
@@ -2,21 +2,29 @@ from requests import request
 
 
 class Spider:
-    TOKEN = "EOk6j38PELxUwJy8"
+    token = "EOk6j38PELxUwJy8"
     virus_url = "https://v2.alapi.cn/api/springTravel/risk"
     healthy_travel_url = "https://v2.alapi.cn/api/springTravel/query"
-    params = {"token": TOKEN}
+    params = {"token": token}
 
     def __init__(self):
         pass
 
-    def get_virus(self, city):
-        self.params.update({"province": city})
-        result = request('get', self.virus_url, params=self.params).json()
+    def get_virus(self, province=None, city=None, county=None):
+        payload = {
+            'token': self.token,
+            'province': province,
+            'city': city,
+            "country": county
+        }
+        headers = {'Content-Type': "application/x-www-form-urlencoded"}
+        result = request('POST', self.virus_url, params=payload, headers=headers).json()
 
         if 10 > len(result['data']['high_list']) > 0:
             high_list = result['data']['high_list']
             msg = ''.join(f"{row['area_name']} - 高风险社区：{len(row['communitys'])}个\n" for row in high_list)
+        elif city is None:
+            msg = f"{province} 存在高风险地区：{result['data']['high_count']}个\n"
         else:
             msg = f"{city} 存在高风险地区：{result['data']['high_count']}个\n"
 
@@ -30,4 +38,4 @@ class Spider:
         print(result)
 
 
-Spider().get_healthy_travel()
+print(Spider().get_virus('江西省'))
