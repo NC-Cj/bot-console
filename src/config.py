810ec9c483f8382a9e0e9eb3715bf9c20cc3548a
cjcls <cj@mosynx.com>
2022-09-30 09:57:37 +0800

    开发分支重构

---
+++ src/config.py
@@ -0,0 +1,12 @@
+TOKEN = "EOk6j38PELxUwJy8"
+
+CONFIG_YML = {
+    'listenRoomList': ['wxid_rfmdl29r87jh22'],
+    'debug': False,
+    'root': './wx/robot.py',
+    'script': {
+        'fileName': './script/spider.py',
+        'bySet': 'BySpiderCommand'
+    },
+    'mysqlConnStr': None
+}
