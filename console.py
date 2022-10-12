e792347906f1a5d3e7887d8e6c82eaed873ff99f
cjcls <cj@mosynx.com>
2022-10-12 15:17:36 +0800

    增加一些定制模板和优化

--- console.py
+++ console.py
@@ -4,29 +4,40 @@ from fire import Fire
 from ruamel import yaml
 
 from src import ROBOT_CONFIGURATION
+from src.tpl import tpl
 
 
 class Cmd(object):
 
     @staticmethod
-    def init(name, debug=True, template='group'):
+    def init(name, debug=True, template='01'):
         """This is where we will create the robot configuration item.
+
+        Examples:
+            1、bot init --name=test1 --debug=False --template=01\n
+            2、bot init --name=test2 --template=03\n
+            3、bot init --name=test3
 
         Args:
             name: Robot name.
             debug: Robot with debugging information display. for example: `True` | `False`
             template: Generated bot template files. for example:\n
-                `01` -> individual\n
-                `02` -> group\n
-                `03` -> individual & group
+                `01` -> listen group\n
+                `02` -> listen individual\n
+                `03` -> listen individual & group
         """
         ROBOT_CONFIGURATION.update({
             'name': name,
             'debug': debug if isinstance(debug, bool) else True,
-            'template': template
+            'template': tpl.get(template).get('name')
         })
         with open(f'./{name}.yml', 'w', encoding='utf-8') as ym:
             yaml.dump(ROBOT_CONFIGURATION, ym, allow_unicode=True, Dumper=yaml.RoundTripDumper)
+
+        tpl_path = tpl.get(template).get('path')
+        with open(tpl_path, 'r', encoding='utf-8') as content:
+            with open(f'./{name}.py', 'w', encoding="utf-8") as f:
+                f.write(content.read())
 
         print('Please wait, the package is downloading...')
         os.makedirs('./images/', exist_ok=True)
---
+++ src/tpl.py
@@ -0,0 +1,5 @@
+tpl = {
+    '01': {'name': 'listen group', 'path': 'src/templates/01.tpl'},
+    '02': {'name': 'listen individual', 'path': 'src/templates/02.tpl'},
+    '03': {'name': 'listen individual & group', 'path': 'src/templates/03.tpl'},
+}
