bd4f1339012d71decda0a0f10157d3c9896b319a
cjcls <cj@mosynx.com>
2022-10-12 12:32:22 +0800

    优化终端命令包装

--- console.py
+++ console.py
@@ -1,89 +1,66 @@
 import os
-import pydoc
-import sys
-from traceback import print_exc
 
+from fire import Fire
 from ruamel import yaml
 
 from src import CONFIG_YML
 
 
-def _init_robot(*args):
-    """
-    Command: bot init [Configuration parameters] [Robot configuration file name]
-    :param args:
-        --debug: 允许查看机器人日志的debug版本
-    """
-    if len(args) >= 2:
-        if args[0] == '--debug':
-            CONFIG_YML.update({'debug': True})
-        else:
-            return pydoc.help(_init_robot)
+class Cmd(object):
 
-    with open(f'./{args[-1]}.yml', 'w', encoding='utf-8') as ym:
-        yaml.dump(CONFIG_YML, ym, allow_unicode=True, Dumper=yaml.RoundTripDumper)
+    @staticmethod
+    def init(name, debug=True, template='group'):
+        """This is where we will create the robot configuration item.
 
-    os.makedirs('./images/', exist_ok=True)
-    print('Please wait, the package is downloading...')
-    os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ -r ./requirements.txt')
-    print('done...')
+        Args:
+            name: Robot name.
+            debug: Robot with debugging information display. for example: `True` | `False`
+            template: Generated bot template files. for example:\n
+                `01` -> individual\n
+                `02` -> group\n
+                `03` -> individual & group
+        """
+        CONFIG_YML.update({
+            'name': name,
+            'debug': debug if isinstance(debug, bool) else True,
+            'template': template
+        })
+        with open(f'./{name}.yml', 'w', encoding='utf-8') as ym:
+            yaml.dump(CONFIG_YML, ym, allow_unicode=True, Dumper=yaml.RoundTripDumper)
 
+        print('Please wait, the package is downloading...')
+        os.makedirs('./images/', exist_ok=True)
+        os.system('pip install -i https://pypi.tuna.tsinghua.edu.cn/simple/ -r ./requirements.txt')
+        print('done...')
 
-def _start_robot(*args):
-    """
-    Command: bot start [Robot name]
-    :param args: Only one parameter is required
-        - The robot name corresponds to the created robot configuration file name
-    """
-    if len(args) >= 2:
-        return pydoc.help(_start_robot)
+    @staticmethod
+    def close():
+        """
+        Terminate the background bot process
+        """
+        os.system('taskill /f /im pythonw')
+        print('机器人终止...')
 
-    file = list(filter(lambda n: n.endswith('.yml'), os.listdir('./')))
-    if file is not None:
-        with open(file[0], 'r', encoding='utf-8') as ym:
-            data = yaml.load(ym, Loader=yaml.Loader)
+    @staticmethod
+    def start():
+        """
+        Starting the robot process
+        """
+        file = list(filter(lambda n: n.endswith('.yml'), os.listdir('./')))
+        if file is not None:
+            with open(file[0], 'r', encoding='utf-8') as ym:
+                data = yaml.load(ym, Loader=yaml.Loader)
 
-        print('机器人启动...')
-        if data['debug']:
-            os.system(f'python {data.get("root")}')
-        else:
-            os.system(f'start pythonw {data.get("root")}')
-
-
-def _close_robot(*args):
-    """
-    Command: bot close
-    """
-    if len(args) >= 2:
-        return pydoc.help(_close_robot)
-
-    os.system('taskill /f /im pythonw')
-    print('机器人终止...')
-
-
-def root(command, param):
-    """
-    Command: bot [command] [param]
-    :param command:
-        - `init`
-        - `start`
-        - `close`
-    :param param: parameters passed to each command
-    """
-    try:
-        by.get(f'{command}')(*param)
-    except TypeError as e:
-        if str(e).endswith('not callable'):
-            pydoc.help(root)
-        else:
-            print_exc()
+            print('机器人启动...')
+            if data['debug']:
+                os.system(f'python {data.get("root")}')
+            else:
+                os.system(f'start pythonw {data.get("root")}')
 
 
-by = {
-    'init': _init_robot,
-    'start': _start_robot,
-    'close': _close_robot,
-}
+def main():
+    Fire(Cmd)
+
 
 if __name__ == '__main__':
-    root(sys.argv[1], sys.argv[2:])
+    main()
--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,12 @@
-from .config import TOKEN, CONFIG_YML
+TOKEN = "EOk6j38PELxUwJy8"
+
+CONFIG_YML = {
+    'listenRoomList': None,
+    'listenWeChatIdList': None,
+    'root': '填写bot启动文件路径（包含文件名）',
+    'script': {
+        'fileName': './script/spider.py',
+        'bySet': 'BySpiderCommand'
+    },
+    'mysqlConnStr': None
+}
