5bbcb1cdae316b2b34e0b0edbaf198ee871393a3
cj <cj@mosynx.com>
2022-10-19 17:27:35 +0800

    plan

--- src/tpl.py
+++ src/tpl.py
@@ -2,4 +2,7 @@ tpl = {
     '01': {'name': 'listen group', 'path': 'src/templates/01.tpl'},
     '02': {'name': 'listen individual', 'path': 'src/templates/02.tpl'},
     '03': {'name': 'listen individual & group', 'path': 'src/templates/03.tpl'},
+    '04': {'name': 'Like some community segments', 'path': 'src/templates/04.tpl'},
 }
+
+# TODO 未来需要增加专精的社区机器人
